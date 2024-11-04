bb12ced553fc329b145f9b2437c968e7507400b4
Chen Mo <chenmo@baai.ac.cn>
2024-11-04 17:00:59 +0800

    Use ROOT_PATH in build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ ROOT_PATH=$(readlink -f "$0")
 # git submodule init && git submodule update
 
 # Build nccl
-cd nccl/
+cd $ROOT_PATH/nccl/
 make -j32 src.build NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_90,code=sm_90"
 
 # Build nccl-tests
-cd nccl-tests/
+cd $ROOT_PATH/nccl-tests/
 make -j32 MPI=1 MPI_HOME=/usr/local/mpi CUDA_HOME=/usr/local/cuda NCCL_HOME=$ROOT_PATH/nccl
 
